f3ba8af7935f00da27eb5cfd55ab44254fd4e7bd
itssanjana25 <sanjanavalasange@gmail.com>
2026-06-01 19:56:29 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,83 @@
+# 1.Arithmetic operators
+
+#addition
+print(10+2)
+
+#subtraction
+print(20-10)
+
+#multiplication
+print(10*2)
+
+#exponential
+print(10**2)
+
+#division
+print(10/3)
+
+#floor division
+print(10//2)
+print(10//3)
+
+#modulus
+print(10%2)
+
+a=10
+b=2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a//b)
+print(a%b)
+print()
+
+
+# 2.Comparision Operator
+
+print( 10==10)
+print( 100==10)
+print( 10>=100)
+print( 10<=150)
+print( 105<=10)
+print( 10!=10)
+print( 10!=140)
+print()
+
+
+# 3.Logical operator
+
+# and
+un = input("Enter Username :")
+pw = input("Enter Password :")
+if un == "Sanjana" and pw == "2526" :
+    print(" Login Successful")
+else :
+    print("Invalid credential")
+
+
+# or
+
+char = input("Enter characters :")
+if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u' :
+    print(" Vowels.")
+
+else :
+    print("Consonants.")
+print()
+
+
+# not
+
+marks=int(input(" Enter your marks :"))
+if not marks < 40 :
+
+    print(" Pass ")
+
+else:
+    print(" Fail ")
+
+
+
+    
